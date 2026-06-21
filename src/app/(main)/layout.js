0e7aed10e9c7c257e490478b817e61242fa206ae
ish <tmdghks0615@gmail.com>
2026-06-21 17:43:59 +0900

    .

---
+++ src/app/(main)/layout.js
@@ -0,0 +1,5 @@
+export default function MainLayout({ children }) {
+    return (
+        <main>{children}</main>
+    );
+}
---
+++ src/app/(main)/page.js
@@ -0,0 +1,8 @@
+import {Button} from "@mui/material";
+
+export default function Home() {
+  return <div>
+    <Button variant='contained'>asd</Button>
+    <span>가나다라</span>
+  </div>;
+}
